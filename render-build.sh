b6b7a5455a609cce17c2c1fa0445713b54b9620c
Nilesh Kumar <137385597+nk9000@users.noreply.github.com>
2024-05-21 19:40:01 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,27 +1,3 @@
 #!/usr/bin/env bash
-# Install necessary dependencies for Puppeteer
-apt-get update
-apt-get install -y wget gnupg ca-certificates
-apt-get install -y \
-    fonts-liberation \
-    libappindicator3-1 \
-    libasound2 \
-    libatk-bridge2.0-0 \
-    libatk1.0-0 \
-    libcups2 \
-    libdbus-1-3 \
-    libdrm2 \
-    libgbm1 \
-    libnspr4 \
-    libnss3 \
-    libxcomposite1 \
-    libxdamage1 \
-    libxrandr2 \
-    xdg-utils \
-    libu2f-udev
-
-# Add Google Chrome's PPA
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-apt-get update
-apt-get install -y google-chrome-stable
+# No longer need to install Chromium using apt-get
+echo "Render build script executed successfully."
