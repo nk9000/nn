5a339a8a484372ecf28454e43302f8e2b1992312
Nilesh Kumar <137385597+nk9000@users.noreply.github.com>
2024-05-21 19:41:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(cors());
 
 // Function to get followers count
 async function getFollowersCount(url) {
-    const browser = await puppeteer.launch({ 
-        headless: true, 
-        args: ['--no-sandbox', '--disable-setuid-sandbox'] 
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
